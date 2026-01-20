88114192eaa88ed1a287579952fbc47a0e631d3b
Brandon Thetford <github@snapsinazfs.com>
2026-01-19 20:33:41 -0700

    Begin wiring up the buttons

--- FFUDevelopment/FFUUI.Core/FFUUI.Core/WingetAppsViewControl.xaml.cs
+++ FFUDevelopment/FFUUI.Core/FFUUI.Core/WingetAppsViewControl.xaml.cs
@@ -10,4 +10,34 @@ public partial class WingetAppsViewControl : UserControl
   }
 
   public ObservableWingetAppCollection Items { get; } = [];
+
+  private void SelectAllCheckBox_Click(object sender, RoutedEventArgs e)
+  {
+    bool desiredState = Items.AllSelected is not true;
+
+    using (Items.BeginRangeOperation())
+    {
+      foreach (WingetApp app in Items)
+      {
+        app.IsSelected = desiredState;
+      }
+    }
+  }
+
+  private void RemoveHighlighted_Click(object sender, RoutedEventArgs e)
+  {
+    if (AppListView.SelectedItems.Count == 0)
+    {
+      return;
+    }
+
+    using (Items.BeginRangeOperation())
+    {
+      for (int i = AppListView.SelectedItems.Count - 1; i >= 0; i--)
+      {
+        int index = AppListView.Items.IndexOf(AppListView.SelectedItems[i]);
+        Items.RemoveAt(index);
+      }
+    }
+  }
 }
